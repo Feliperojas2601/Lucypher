853fc18288554a45c839bac729ca1f0b37e7d8b8
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-30 22:37:57 -0500

    Refactor crypto details component: Implement null check for algorithm data, enhance loading state handling, and update component initialization logic to improve user experience and navigation.

--- src/app/pages/cryptoDetails/cryptoDetails.component.ts
+++ src/app/pages/cryptoDetails/cryptoDetails.component.ts
@@ -1,19 +1,35 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, signal, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CryptoService } from '../../services/crypto.service';
 import { GistComponent } from '../../components/gist/gist.component';
+import { CryptoAlgorithm, CryptoAlgorithmName } from '../../interfaces/cryptoAlgorithm.interface';
+
 @Component({
   selector: 'app-crypto-details',
   imports: [GistComponent],
   templateUrl: './cryptoDetails.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CryptoDetailsComponent {
+export class CryptoDetailsComponent implements OnInit {
+    router = inject(Router);
     activatedRoute = inject(ActivatedRoute);
     cryptoService = inject(CryptoService);
 
-    algorithmName = this.activatedRoute.snapshot.params['algorithmName'];
-    algorithm = this.cryptoService.getCryptoAlgorithmDetails(this.algorithmName);
-    
+    algorithmName: CryptoAlgorithmName | null = null;
+    algorithm: CryptoAlgorithm | null = null;
     isLoading = signal<boolean>(false);
+
+    ngOnInit() {
+        const name = this.activatedRoute.snapshot.params['algorithmName'];
+        try {
+            this.algorithmName = name as CryptoAlgorithmName;
+            if (this.algorithmName) {
+                this.algorithm = this.cryptoService.getCryptoAlgorithmDetails(this.algorithmName);
+            } else {
+                this.router.navigate(['/']);
+            }
+        } catch (error) {
+            this.router.navigate(['/']);
+        }
+    }
 }
